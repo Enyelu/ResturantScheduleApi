78202d2008e307da0776d870188bc784ea7874dd
Chukwuebuka Enyelu <cenyelu@axxess.com>
2022-03-03 16:24:12 +0100

    Domain class library, entities/models created

---
+++ ResturantScheduling.Domain/Entities/OpenClose.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResturantScheduling.Domain.Entities
+{
+    public class OpenClose
+    {
+        public string? Type { get; set; }
+        public int Value { get; set; }
+    }
+}
---
+++ ResturantScheduling.Domain/Entities/OpenCloseRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace ResturantScheduling.Domain.Entities
+{
+    public class OpenCloseRequest
+    {
+        public List<OpenClose>? Monday { get; set; }
+        public List<OpenClose>? Tuesday { get; set; }
+        public List<OpenClose>? Wednesday { get; set; }
+        public List<OpenClose>? Thursday { get; set; }
+        public List<OpenClose>? Friday { get; set; }
+        public List<OpenClose>? Saturday { get; set; }
+        public List<OpenClose>? Sunday { get; set; }
+    }
+}
